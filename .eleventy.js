526198638e6a0fe434fcf7663b8ec825b5846223
Francois Buys <buys.fran@gmail.com>
2020-12-13 19:02:12 +0200

    🔧 Add initial scaffolding and configuration
    With lots of help from:
     - https://github.com/dafiulh/vredeburg
     - https://github.com/philhawksworth/eleventyone
     - https://github.com/5t3ph
     - https://www.11ty.dev/docs/
    
    Prettier keeps our code nicely formatted.
    Tailwind is an amazing css framework.
    Rimraf ensures we get rid of previously compiled files.

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+module.exports = (config) => {
+  config.addLayoutAlias('base', 'layouts/base.njk')
+
+  // minify the html output
+  config.addTransform('htmlmin', require('./src/utils/minify-html.js'))
+
+  // compress and combine css files
+  const pluginTailwind = require('eleventy-plugin-tailwindcss')
+  config.addPlugin(pluginTailwind, {
+    src: "src/css/styles.css",
+    dest: ".",
+    keepFolderStructure: false,
+    minify: true
+  })
+
+  // compress and combine js files
+  config.addFilter('jsmin', function (code) {
+    const UglifyJS = require('uglify-js')
+    let minified = UglifyJS.minify(code)
+    if (minified.error) {
+      console.log('UglifyJS error: ', minified.error)
+      return code
+    }
+    return minified.code
+  })
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'public',
+    },
+  }
+}
