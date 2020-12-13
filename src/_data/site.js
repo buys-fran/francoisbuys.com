fe7015076f8e6bb4be23296e83e92228fd3e809e
Francois Buys <buys.fran@gmail.com>
2020-12-13 19:02:12 +0200

    🔧 Add initial scaffolding and configuration
    With lots of help from:
     - https://github.com/dafiulh/vredeburg
     - https://github.com/philhawksworth/eleventyone
     - https://github.com/5t3ph
     - https://www.11ty.dev/docs/
    
    Prettier keeps our code nicely formatted.
    Tailwind is an amazing css framework.
    Rimraf ensures we get rid of previously compiled files.

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Francois Buys - Fullstack Developer',
+  url: `${process.env.URL}`,
+  authorName: 'Francois Buys',
+  authorUrl: 'https://francoisbuys.com',
+  description:
+    "francoisbuys.com is a technical blog where software tips and ideas are shared from a fullstack developer's perspective",
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.njk', './src/**/*.svg', './src/**/*.html'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Barlow Condensed', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
