69569dba146dc78e65b18830321a8b1566ed3b51
Francois Buys <buys.fran@gmail.com>
2021-08-24 10:44:35 +0200

    ✨ Add RSS
    Guided by: https://piccalil.li/course/learn-eleventy-from-scratch/lesson/17/
    
    An RSS feed is a way for users to subscribe to updates from your
    website.
    
    RSS can also be used to post content to social networks, using tools like IFTTT.
    
    Someone can add any page on your site to their RSS reader and it will
    pick up the link to your feed automatically

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@
 const dateFilter = require('./src/filters/date-filter.js')
 const w3DateFilter = require('./src/filters/w3-date-filter.js')
 
+// Plugins
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 // Utils
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js')
 
@@ -36,6 +39,9 @@ module.exports = (config) => {
   // Add filters
   config.addFilter('dateFilter', dateFilter)
   config.addFilter('w3DateFilter', w3DateFilter)
+
+  // Add plugins
+  config.addPlugin(rssPlugin);
 
   return {
     markdownTemplateEngine: 'njk',
