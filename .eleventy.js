fdd39d12dc335ac7b36d5b56943eed4fe7a19cba
Francois Buys <buys.fran@gmail.com>
2022-05-11 23:49:35 +0200

    Reset everything

--- .eleventy.js
+++ .eleventy.js
@@ -1,55 +1,8 @@
-// Filters
-const dateFilter = require('./src/filters/date-filter.js')
-const w3DateFilter = require('./src/filters/w3-date-filter.js')
-
-// Plugins
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
-
-// Utils
-const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js')
-
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/images/')
-
-  config.setFrontMatterParsingOptions({
-    excerpt: true,
-    // Optional, default is "---"
-    excerpt_separator: '<!-- more -->',
-  })
-
-  // Blog posts in reverse date order
-  config.addCollection('blog', (collection) => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse()
-  })
-
-  // Digital collections (bookmarks) in reverse date order
-  config.addCollection('bookmarks', (collection) => {
-    return [...collection.getFilteredByGlob('./src/bookmarks/*.md')].reverse()
-  })
-
-  // Featured blog posts in ordered by display order
-  config.addCollection('featuredPosts', (collection) => {
-    const posts = collection
-      .getFilteredByGlob('./src/posts/*.md')
-      .filter((x) => x.data.featured)
-    return sortByDisplayOrder(posts)
-  })
-
-  // Add filters
-  config.addFilter('dateFilter', dateFilter)
-  config.addFilter('w3DateFilter', w3DateFilter)
-
-  // Add plugins
-  config.addPlugin(rssPlugin);
-
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
     dir: {
       input: 'src',
-      output: 'dist',
-    },
+      output: 'public',
+    }
   }
 }
--- src/bookmarks/august-2021-links.md
+++
@@ -1,9 +0,0 @@
----
-title: '🔖 August 2021 Bookmarks'
-date: '2021-08-01'
----
-
-# React + Typescript Links
-https://react-redux.js.org/introduction/getting-started
-https://www.freecodecamp.org/news/learn-typescript-with-this-crash-course/
-https://blog.isquaredsoftware.com/2019/11/blogged-answers-learning-and-using-typescript/
